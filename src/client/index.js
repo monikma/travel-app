8a5971b53c27ed23d0420bf4a3bffc1b59c0e413
Monika Machunik <monika.machunik@gmail.com>
2020-05-21 01:34:40 +0200

    Add: Mechanism to export functions as Client library

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,10 @@
+//import { performAction } from './js/app.js'
 import './styles/main.scss'
 import './js/app.js'
 
 console.log("Built with Webpack");
+
+// exports for 'Client' library:
+export {
+    //performAction
+}
