811aa7b5a2d73af87caddcff30b7f62dd2fe7298
Monika Machunik <monika.machunik@gmail.com>
2020-05-21 06:59:10 +0200

    Bug: Await for images

--- src/client/js/pixabay.js
+++ src/client/js/pixabay.js
@@ -3,7 +3,7 @@ const pixabayApiKey= '&key=16662631-9cbc21132648511fe8b2b6986'
 
 // NOTE: You will need Access-Control-Allow-origin plugin to ignore CORS errors
 const fetchImage = async (city, countryFallback) => {
-    const imgURL = fetchImg(pixabayURL, city, pixabayApiKey)
+    const imgURL = await fetchImg(pixabayURL, city, pixabayApiKey)
     if (imgURL) {
         return imgURL
     } else { // country fallback
